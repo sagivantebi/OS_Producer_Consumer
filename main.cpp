b4a5b6452771b67bb13802db1a3fcdb7e3bb1874
Sagiv Antebi <84729141+sagivantebi@users.noreply.github.com>
2022-06-03 17:08:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include <thread>
 #include "UnboundedQ.h"
 #include "BoundedQ.h"
+#include <fstream>
+#include <string>
 
-#define N 5
-#define NUM_ARTICLES 5
-#define BOUNDED 3
+#define N 3
+#define NUM_ARTICLES 3
+#define BOUNDED 1
 #define DONE "DONE"
 #define SPORTS "SPORTS"
 #define NEWS "NEWS"
@@ -12,107 +13,176 @@
 
 using namespace std;
 
-
-string typeArticle[3] = {SPORTS, NEWS, WHETHER};
+struct producerArgs {
+    int id;
+    int numArticles;
+    int boundedProducer;
+};
 
 vector<BoundedQ> producers;
 UnboundedQ SportsQ = UnboundedQ();
 UnboundedQ NewsQ = UnboundedQ();
 UnboundedQ WhetherQ = UnboundedQ();
-BoundedQ CoPQ = BoundedQ(BOUNDED * 3);
-
-
-void createProducerBoundedQ(int num, int bounded) {
-    for (int i = 0; i < num; ++i) {
-        auto newBQ = BoundedQ(bounded);
-        producers.push_back(newBQ);
-    }
-}
-
-void addProducerArticle(int id, string typeArt, int counter) {
-    string s = "Producer " + to_string(id) + " " + typeArt + " " + to_string(counter);
-    cout << s << endl;
-    producers.at(id).insert(s);
-}
+BoundedQ CoPQ = BoundedQ(BOUNDED);
+sem_t fullCoPQ;
 
 void Producer(int id, int numberArticles) {
+    string s;
     int cSport = 0, cNews = 0, cWhether = 0;
-    for (int i = 0; i < numberArticles; ++i) {
-        string typeArt = typeArticle[rand() % 3];
-        if (typeArt == SPORTS) {
+    int typeArt;
+    int i;
+    for (i = 0; i < numberArticles - 1; ++i) {
+        typeArt = rand() % 3;
+        if (typeArt == 0) {
+            s = "Producer " + to_string(id) + " " + SPORTS + " " + to_string(cSport);
+            producers[id].insert(s);
             cSport++;
-            addProducerArticle(id, typeArt, cSport);
-        }
-        if (typeArt == NEWS) {
+        } else if (typeArt == 1) {
+            s = "Producer " + to_string(id) + " " + NEWS + " " + to_string(cNews);
+            producers[id].insert(s);
             cNews++;
-            addProducerArticle(id, typeArt, cNews);
         } else {
+            s = "Producer " + to_string(id) + " " + WHETHER + " " + to_string(cWhether);
+            producers[id].insert(s);
             cWhether++;
-            addProducerArticle(id, typeArt, cWhether);
         }
     }
-    producers.at(id).insert(DONE);
+    producers[id].insert(DONE);
 }
 
-bool checkIfAllProducersEmpty() {
-    for (int i = 0; i < N; ++i) {
-        if (!producers[i].isEmpty()) {
-            return false;
-        }
-    }
-    return true;
-}
 
 void Dispatcher() {
-    int count = 0;
-    string s;
-    while (1) {
-        if (!checkIfAllProducersEmpty()) {
-            for (int i = 0; i < N; ++i) {
-                if (producers[i].isEmpty()) {
-                    continue;
-                }
-                s = "";
-                s = producers[i].remove();
-                if (s.find(SPORTS) != std::string::npos) {
-                    SportsQ.insert(s);
-                    cout << "Inserted to the  “S dispatcher queue”" << endl;
-                } else if (s.find(NEWS) != std::string::npos) {
-                    NewsQ.insert(s);
-                    cout << "Inserted to the  “N dispatcher queue”" << endl;
-                } else if (s.find(WHETHER) != std::string::npos) {
-                    WhetherQ.insert(s);
-                    cout << "Inserted to the  “W dispatcher queue”" << endl;
-                } else if (s == DONE) {
-                    producers[i].emptyDone();
+    string sDisp;
+    int i;
+    bool allEmpty = false;
+    while (!allEmpty) {
+        for (i = 0; i < N; ++i) {
+            if (!producers[i].isEmpty()) {
+                break;
+            }
+            allEmpty = true;
+        }
+        if (!allEmpty) {
+            for (i = 0; i < N; ++i) {
+                if (!producers[i].isEmpty()) {
+                    sDisp = producers[i].remove();
+                    if (sDisp.find(SPORTS) != std::string::npos) {
+                        SportsQ.insert(sDisp);
+                    } else if (sDisp.find(NEWS) != std::string::npos) {
+                        NewsQ.insert(sDisp);
+                    } else if (sDisp.find(WHETHER) != std::string::npos) {
+                        WhetherQ.insert(sDisp);
+                    } else if (sDisp == DONE) {
+                        producers[i].emptyDone();
+                    }
                 }
             }
         }
-        else{
-            SportsQ.insert(DONE);
-            NewsQ.insert(DONE);
-            WhetherQ.insert(DONE);
-            cout<<DONE<<"\n\n\n\n";
-            break;
-        }
     }
+    SportsQ.insert(DONE);
+    NewsQ.insert(DONE);
+    WhetherQ.insert(DONE);
 }
 
 void CoEditors(int id) {
-    cout << "My CoEditors id is " << id << endl;
+    switch (id) {
+        case 1: {
+            string s1;
+            s1 = SportsQ.remove();
+            while (!SportsQ.isEmpty()) {
+                CoPQ.insert(s1);
+                s1 = SportsQ.remove();
+                if (s1 == DONE)
+                    SportsQ.emptyDone();
+            }
+            CoPQ.insert(DONE);
+            break;
+        }
+        case 2: {
+            string s2;
+            s2 = NewsQ.remove();
+            while (!NewsQ.isEmpty()) {
+                CoPQ.insert(s2);
+                s2 = NewsQ.remove();
+                if (s2 == DONE)
+                    NewsQ.emptyDone();
+            }
+            CoPQ.insert(DONE);
+            break;
+        }
+        case 3: {
+            string s3;
+            s3 = WhetherQ.remove();
+            while (!WhetherQ.isEmpty()) {
+                CoPQ.insert(s3);
+                s3 = WhetherQ.remove();
+                if (s3 == DONE)
+                    WhetherQ.emptyDone();
+            }
+            CoPQ.insert(DONE);
+            break;
+        }
+        default:
+            cout << "default" << endl;
+    }
 }
 
 
 void ScreenManager() {
-    cout << "My ScreenManager" << endl;
+    string sScreenManager;
+    int num;
+    sScreenManager = CoPQ.remove();
+    while (!CoPQ.isEmpty()) {
+        if (sScreenManager == DONE) {
+            sem_wait(&fullCoPQ);
+            sem_getvalue(&fullCoPQ, &num);
+            if (num == 0) {
+                CoPQ.emptyDone();
+                break;
+            }
+        } else
+            cout << sScreenManager << endl;
+        sScreenManager = CoPQ.remove();
+    }
 }
 
 
-int main() {
-    createProducerBoundedQ(N, BOUNDED);
+
+vector<producerArgs> readConfig(char *path) {
+    string myConfig;
+    vector<producerArgs> vProducers;
+    string id, numArticles, boundedProducers, boundedCoEditors, newLine;
+    bool reading = true;
+    ifstream f(path);
+    getline(f, myConfig);
+    while (f) {
+        getline(f, id);
+        getline(f, numArticles);
+        getline(f, boundedProducers);
+        getline(f, newLine);
+        if (equal(numArticles.begin(), numArticles.end(), "")) {
+            CoPQ = BoundedQ(stoi(id));
+        } else {
+            producerArgs p{stoi(id), stoi(numArticles), stoi(boundedProducers)};
+            vProducers.push_back(p);
+        }
+        id.clear();
+        numArticles.clear();
+        boundedProducers.clear();
+    }
+    f.close();
+    return vProducers;
+}
+
+int main(int argc, char *argv[]) {
     vector<thread> tProducers = vector<thread>();
-    for (int i = 0; i < N; ++i) {
-        thread t(Producer, i, NUM_ARTICLES);
+    int i, j;
+    vector<producerArgs> vProducers = readConfig(argv[1]);
+    sem_init(&fullCoPQ, 0, vProducers.size());
+    for (auto &it: vProducers) {
+        auto newBQ = BoundedQ((it.boundedProducer));
+        producers.push_back(newBQ);
+        thread t(Producer, it.id - 1, it.numArticles);
         tProducers.push_back(move(t));
     }
     thread dispatcher(Dispatcher);
@@ -120,7 +190,8 @@ int main() {
     thread coEditors2(CoEditors, 2);
     thread coEditors3(CoEditors, 3);
     thread screenManager(ScreenManager);
-    for (int i = 0; i < N; ++i) {
+    //waiting for the threads to finish
+    for (i = 0; i < N; ++i) {
         tProducers[i].join();
     }
     dispatcher.join();
